068b179cb44501a05ca8611fae6747ac244516b4
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-24 20:18:08 +0530

    concept two actionsheet

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,21 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
-        let blur = Binding<CGFloat>(get: {
-            self.blurAmount
-        }, set: {
-            self.blurAmount = $0
-            print("New value is \(self.blurAmount)")
-        })
-        
-        return VStack {
-            Text("Hello, World!")
-            .blur(radius: blurAmount)
-            
-            Slider(value: blur,in: 0...20)
+        Text("Hello World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet.toggle()
+        }
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                .default(Text("Red")) { self.backgroundColor = .red },
+                .default(Text("Blue")) { self.backgroundColor = .blue },
+                .default(Text("Green")) { self.backgroundColor = .green },
+                .cancel()
+            ])
         }
     }
 }
